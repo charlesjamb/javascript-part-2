a0deddcbccb01c73a4c7d19a55ac2e181fa1f287
Charles JAMBON <charles.jambon@gmail.com>
2016-10-05 21:39:25 +0000

    finished warrior

--- warriors.js
+++ warriors.js
@@ -7,18 +7,21 @@ function Warrior(name, gender) {
     this.level = 1;
     this.weapon = 'wooden sword';
     this.power = Math.random()*(100-1)+1;
-    this.fight = function() {
-        console.log('rushes to the arena with');
-    };
-    this.faceoff = function(opponent) {
-        if (this.power > opponent.power) {
-            console.log(this.name + ' wins!');
-        }
-        else {
-            console.log(opponent.name + ' wins!');
-        }
-    };
-}
+};
+    
+Warrior.prototype.fight = function(opponent) {
+    return this.name + ' rushes to the arena with ' + opponent.name;
+};
+
+Warrior.prototype.faceoff = function(opponent) {
+    if (this.power > opponent.power) {
+        return this.name + ' wins!';
+    }
+    else {
+        return opponent.name + ' wins!';
+    }
+};
+
 
 var chuck = new Warrior('Chuck', 'M');
 var eve = new Warrior('Eve', 'F');
@@ -26,3 +29,7 @@ var reese = new Warrior('Reese', 'M');
 var aly = new Warrior('Aly', 'F');
 var ian = new Warrior('Ian', 'M');
 
+var enterTheArena = chuck.fight(eve);
+var theBattle = chuck.faceoff(eve);
+//console.log(enterTheArena);
+//console.log(theBattle);
