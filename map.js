537b8c3e7ec41996b4e342d4287b19b558f2e17e
Charles JAMBON <charles.jambon@gmail.com>
2016-10-06 04:03:59 +0000

    getWalkableOutput

--- map.js
+++ map.js
@@ -1,6 +1,7 @@
 function randomTypes() {
     var typesAvailable = ['grass', 'sand', 'water']
-    var random = (Math.random() * 2);
+    var random = (Math.random() * 3);
+    //console.log(random);
     var out = typesAvailable[Math.trunc(random)];
     return out;
 }
@@ -13,7 +14,19 @@ function Tile(type) {
         }
         else {return false;}
     };
+    this.getWalkableOutput = function(){
+      if (this.isWalkable()){
+        return 'O';
+      }
+      else{
+        return 'X';
+      }
+    };
+    this.getAsciiOutput = function() {
+        
+    };
 };
+
 
 function Map(width, height) {
     this.width = parseInt(width);
@@ -26,12 +39,23 @@ function Map(width, height) {
         //console.log(i);
         for (var j = 0; j <= this.width; j++) {
             //console.log(j);
-            row.push(new Tile('grass'));
+            row.push(new Tile(randomTypes()));
         }
         this.tiles.push(row);
         //console.log(row.length);
     }
+    this.getWalkableOutput = function() {
+        for (var i = 0; i <= this.height; i++) {
+            var row = "";
+            for (var j = 0; j <= this.width; j++) {
+                row += this.tiles[i][j].getWalkableOutput();
+            }
+            console.log(row);
+        }
+    };
 };
 
-var test = new Map(10, 10);
-console.log(test);
+
+
+var test = new Map(4, 4);
+test.getWalkableOutput();
